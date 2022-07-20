3010f0bda0719699b8e3b60f3f9022d08396f6b8
Jonathan Bouvette <jonathan.bouvette@nih.gov>
2022-07-20 15:10:33 -0400

    Adding square recentering method and test.

--- Smartscope/core/test_commands.py
+++ Smartscope/core/test_commands.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
 import torch
+from Smartscope.core.microscope_interfaces import SerialemInterface
 
 from Smartscope.lib.preprocessing_methods import process_hm_from_frames
 
@@ -27,3 +28,15 @@ def test_high_mag_frame_processing(test_dir=Path(os.getenv('AUTOSCREENDIR'), 'te
     frames_dirs = [Path(os.getenv('AUTOSCREENDIR')), Path(os.getenv('TEST_FILES'), 'highmag_frames')]
     movie = process_hm_from_frames(name, frames_file_name=frames_file_name, frames_directories=frames_dirs)
     print(f'All movie data: {movie.check_metadata()}')
+
+
+def test_realign_to_square(microscope_id):
+    from Smartscope.core.models import Microscope
+    microscope = Microscope.objects.get(pk=microscope_id)
+    with SerialemInterface(ip=microscope.serialem_IP,
+                           port=microscope.serialem_PORT,
+                           directory=microscope.windows_path,
+                           scope_path=microscope.scope_path,
+                           energyfilter=False,
+                           loader_size=microscope.loader_size) as scope:
+        scope.realign_to_square()
