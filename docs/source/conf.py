440cde52f5e6e5f9a745c58e29dc5bd84e40c8c9
Jonathan Bouvette <jonathan.bouvette@nih.gov>
2022-09-16 17:22:09 -0400

    Update to docs to reflect public access

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -61,3 +61,10 @@ html_theme = 'sphinx_book_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_theme_options = {
+
+    "repository_url": "https://github.com/NIEHS/SmartScope",
+    "use_repository_button": True,
+    "use_issues_button": True,
+
+}
