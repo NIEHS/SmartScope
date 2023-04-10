b248dc037412e9968165ba87d991f36ff71ff389
Jonathan Bouvette <jonathan.bouvette@nih.gov>
2023-04-10 11:43:57 -0400

    Segregated default and custom protocols/plugins

--- Smartscope/core/settings/worker.py
+++ Smartscope/core/settings/worker.py
@@ -3,17 +3,23 @@ from pathlib import Path
 from Smartscope.core.config import register_plugins, register_protocols, register_external_plugins, get_active_plugins_list, get_protocol_commands
 from Smartscope.core.metadata_viewer import CTFFitViewer
 
-SMARTSCOPE_CONFIG = Path(os.getenv('CONFIG'))
-SMARTSCOPE_PLUGINS = SMARTSCOPE_CONFIG / 'plugins'
+SMARTSCOPE_CUSTOM_CONFIG = Path(os.getenv('CONFIG'))
+SMARTSCOPE_DEFAULT_CONFIG = Path('/opt/smartscope/config/smartscope/')
+
+SMARTSCOPE_DEFAULT_PLUGINS = SMARTSCOPE_DEFAULT_CONFIG / 'plugins'
+SMARTSCOPE_CUSTOM_PLUGINS = SMARTSCOPE_CUSTOM_CONFIG / 'plugins'
 EXTERNAL_PLUGINS_DIRECTORY = Path(os.getenv('EXTERNAL_PLUGINS_DIRECTORY'))
-EXTERNAL_PLUGINS_LIST:list = get_active_plugins_list(EXTERNAL_PLUGINS_DIRECTORY, SMARTSCOPE_PLUGINS / 'external_plugins.txt')
-SMARTSCOPE_PROTOCOLS = SMARTSCOPE_CONFIG / 'protocols'
+EXTERNAL_PLUGINS_LIST:list = get_active_plugins_list(EXTERNAL_PLUGINS_DIRECTORY, SMARTSCOPE_CUSTOM_CONFIG / 'external_plugins.txt')
+
+SMARTSCOPE_CUSTOM_PROTOCOLS = SMARTSCOPE_CUSTOM_CONFIG / 'protocols'
+SMARTSCOPE_DEFAULT_PROTOCOLS = SMARTSCOPE_DEFAULT_CONFIG / 'protocols'
 
 ##Register plugins
 PLUGINS_FACTORY = dict()
-register_plugins(SMARTSCOPE_PLUGINS, PLUGINS_FACTORY)
+register_plugins([SMARTSCOPE_DEFAULT_PLUGINS,SMARTSCOPE_CUSTOM_PLUGINS], PLUGINS_FACTORY)
 PROTOCOLS_FACTORY = dict()
-register_protocols(SMARTSCOPE_PROTOCOLS, PROTOCOLS_FACTORY)
+
+register_protocols([SMARTSCOPE_DEFAULT_PROTOCOLS, SMARTSCOPE_CUSTOM_PROTOCOLS], PROTOCOLS_FACTORY)
 register_external_plugins( EXTERNAL_PLUGINS_LIST, plugins_factory=PLUGINS_FACTORY, protocols_factory=PROTOCOLS_FACTORY )
 PLUGINS_FACTORY['CTF Viewer'] = CTFFitViewer()
 
