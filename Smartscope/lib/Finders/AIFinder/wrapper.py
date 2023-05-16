755fa732464f1487f605fcf7957072679424ec38
Jonathan Bouvette <jonathan.bouvette@nih.gov>
2023-05-16 14:20:31 -0400

    AI picker can now pick med mag The ability to use the AI hole picker was added. The picks are not very well centered currently but the initial work is done.

--- Smartscope/lib/Finders/AIFinder/wrapper.py
+++ Smartscope/lib/Finders/AIFinder/wrapper.py
@@ -1,5 +1,7 @@
 import os
 import sys
+import cv2
+import numpy as np
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'detectors'))
 
 from .detectors.detect_squares import detect
@@ -7,6 +9,9 @@ from .detectors.detect_holes import detect_holes, detect_and_classify_holes
 from ..basic_finders import find_square_center
 import logging
 import torch
+
+from Smartscope.lib.image_manipulations import fourier_crop
+from Smartscope.lib.montage import Montage
 # logger = logging.getLogger('processing')
 # logger = logging.getLogger('autoscreen')
 logger = logging.getLogger(__name__)
@@ -30,18 +35,31 @@ def find_squares(montage, **kwargs):
     return (squares, labels), success, dict()
 
 
-def find_holes(montage, **kwargs):
+def find_holes(montage:Montage, **kwargs):
     logger.info('Running AI hole detection')
     # centroid = find_square_center(montage.image)
     kwargs['weights_circle'] = os.path.join(WEIGHT_DIR, kwargs['weights_circle'])
     if not IS_CUDA:
         kwargs['device'] = 'cpu'
-    holes, _ = detect_holes(montage.image, **kwargs)
+    image = montage.image
+    binning=1
+    if montage.pixel_size < 100:
+        logger.info(f'Resizing image')
+        binning = (150/montage.pixel_size)
+        image = fourier_crop(image, height=montage.shape_y/binning)
+        pad_x = int((montage.shape_x - image.shape[0]) //2)
+        pad_y = int((montage.shape_y - image.shape[1]) //2)
+        image = cv2.copyMakeBorder(image,pad_x,pad_x,pad_y,pad_y,cv2.BORDER_CONSTANT,value=0)
+    logger.debug(f'Resized shape: {image.shape}, original: {montage.image.shape}')
+    holes, _ = detect_holes(image, **kwargs)
+    logger.info(f'AI hole detection found {len(holes)} holes')
     success = True
     if len(holes) < 10:
         success = False
-    # holes = [i.numpy() for i in holes]
-    logger.info(f'AI hole detection found {len(holes)} holes')
+    logger.debug(f'{holes[0]},{type(holes[0])}')
+    
+    holes = [(np.array(hole)-np.array(list(montage.center)*2))*binning + np.array(list(montage.center)*2) for hole in holes]
+    logger.debug(f'{holes[0]},{type(holes[0])}')
     return holes, success, dict()
 
 
