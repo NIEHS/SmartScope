1ea870791c4522292a9ce4e98ca7b2243dc14916
Jonathan <jonathan.bouvette@nih.gov>
2023-10-10 17:47:36 +0000

    updated tooltips

--- Smartscope/server/frontend/forms.py
+++ Smartscope/server/frontend/forms.py
@@ -122,9 +122,9 @@ class GridCollectionParamsForm(forms.ModelForm):
             tilt_angle='Tilt angle. For tilted data collection.',
             save_frames='Save the frames for high-mag acquisition or just the aligned sum if unchecked. The Session needs to be stopped and restarted for this change to take effect',
             zeroloss_delay='Delay in hours for the zero loss peak refinement procedure. Only takes effect if detector has an energy filter. Use -1 to deactivate',
-            hardwaredark_delay= 'Delay in hours for the zero loss peak refinement procedure. Only takes effect if detector has an energy filter. Use -1 to deactivate',
-            offset_targeting='Enable random targeting off-center to sample the ice gradient and carbon mesh particles. Automatically disabled in data collection mode.',
-            offset_distance='Override the random offset by an absolute value in microns. Can be used in data collection mode. Use -1 to disable',
+            hardwaredark_delay= 'Delay in hours for the hardware dark acquisition. Use -1 to deactivate',
+            offset_targeting='Enable targeting off-center to sample the ice gradient and carbon mesh particles. Use the Offset Distance setting to change behavior. Disabled in data collection mode unless offset distance is set.',
+            offset_distance='Set a fixed offset value in microns. During screening, use -1 for a random offset dependent of the hole size. During data collection, only fixed values are allowed.',
             multishot_per_hole='Enable multishot per hole.'
         )
 
